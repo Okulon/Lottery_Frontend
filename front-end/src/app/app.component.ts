16f9884cc76b38de6be1b740f174f625883ef432
Amir0reza <white_mage@Amir>
2022-11-26 22:39:18 +0100

    initialize the angular

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,16 +1,60 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
 import { HttpClient } from "@angular/common/http"
 import { ethers } from "ethers"
 
 import LotteryJson from "../assets/Lottery.json"
 import ballotJson from "../assets/LotteryTokenClassic.json"
 
+declare global {
+    interface Window {
+        ethereum: any
+    }
+}
+
+export class eventsClass {
+    Winner: string
+    Prize: string
+
+    constructor(_winner: string, _prize: string) {
+        this.Winner = _winner
+        this.Prize = _prize
+    }
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'front-end';
+    wallet: ethers.Wallet | ethers.providers.JsonRpcSigner | undefined
+    walletAddress: string | undefined
+    provider:
+        | ethers.providers.BaseProvider
+        | ethers.providers.Web3Provider
+        | undefined
+
+    constructor(private http: HttpClient) {}
+
+    connectMetamask() {
+        if (typeof window.ethereum !== "undefined") {
+            try {
+                let accounts: ethers.providers.JsonRpcSigner[]
+                window.ethereum
+                    .request({ method: "eth_requestAccounts" })
+                    .then(() => {
+                        window.ethereum
+                            .request({ method: "eth_accounts" })
+                            .then((ans: ethers.providers.JsonRpcSigner[]) => {
+                                accounts = ans
+                                this.wallet = accounts[0]
+                            })
+                    })
+            } catch (error) {
+                console.log(error)
+            }
+        } else {
+            console.log("Error connecting to metamask !!!")
+        }
+    }
 }
